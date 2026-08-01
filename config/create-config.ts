b5c733198828463f4d1c78a3eb72749bd4b0cba5
amine guissouma <secretwars2019@gmail.com>
2026-08-01 15:20:22 +0200

    feat apikit: centralisation de la configuration

---
+++ config/create-config.ts
@@ -0,0 +1,36 @@
+import {ErrorLevelVisibility} from "../errors/enum/ErrorLevelVisibility";
+import {UiErrorConfig, UIErrorConfigRegistry} from "../errors/errorHandler/ui-config-registry";
+import {ErrorCategory} from "../errors/enum/ErrorCategory";
+import {UIErrorLevelRegistry} from "../errors/errorHandler/ui-error-level-manager";
+import {BusinessErrorRegistry} from "../errors/business-error-codes";
+
+export interface ApikitConfig {
+
+    baseURL?: string;
+
+    visibility?: ErrorLevelVisibility;
+
+    ui?: UiErrorConfig;
+
+    businessErrors?: Record<string, ErrorCategory>;
+
+}
+
+
+export function configureApikit(config: ApikitConfig): void {
+
+    if (config.baseURL) {
+        // todo faire un registre api.defaults.baseURL = config.baseURL;
+    }
+    if (config.visibility) {
+        UIErrorLevelRegistry.set(config.visibility);
+    }
+
+    if (config.ui) {
+        UIErrorConfigRegistry.set(config.ui);
+    }
+
+    if (config.businessErrors) {
+        BusinessErrorRegistry.set(config.businessErrors);
+    }
+}
