5a8d3cb0fc3ed2383016081773e9971dc3ce8ff4
amine guissouma <secretwars2019@gmail.com>
2026-08-03 11:21:36 +0200

    reacfacto apikit: separation des responsabilité d'executilon des process de requete et d'Erreur

--- public/apikit-axio-client.ts
+++ public/apikit-axio-client.ts
@@ -1,78 +1,97 @@
-import {AxiosRequestConfig, AxiosResponse} from "axios";
+import {AxiosRequestConfig} from "axios";
+import {z} from "zod";
+
 import {api} from "../http/axios/axio-client";
 import {apikitUnwrapper} from "../api/apikit-unwrapper";
 import {ApikitException} from "../api/apikit-exception";
 import {handleGlobalError} from "../errors/error-handler";
 import {errorNormalizer} from "../errors/error-normalizer";
-import { z } from 'zod';
 import {shouldHandleGlobalError} from "../errors/should-handle-global-error";
 import {ApikitHookRegistry} from "../config/apikit-hook-registry";
 
-export interface RequestOptions extends Omit<AxiosRequestConfig, "data"> {
+
+export interface RequestOptions
+    extends Omit<AxiosRequestConfig, "data"> {
+
     payload?: unknown;
 }
 
+
+
+/** Exécute une la pipline de la requête Apikit complète*/
+
+async function executeRequest<TResponse>(
+    requestConfig: AxiosRequestConfig,
+    schema?: z.ZodSchema<TResponse>
+): Promise<TResponse> {
+    const hooks = ApikitHookRegistry.get();
+
+    // Point d'extension utilisateur avant l'envoi de la requête
+    let finalConfig = requestConfig;
+    for(const hook of hooks.beforeRequest ?? []){
+        finalConfig = await hook(finalConfig);
+    }
+    // Exécution de la requête HTTP
+    let response = await api.request(finalConfig);
+
+    // Point d'extension utilisateur après une réponse réussie
+
+    for(const hook of hooks.afterResponse ?? []){
+        response = await hook(response);
+    }
+
+    // Recupere la reponse de la requette
+    const data:TResponse = apikitUnwrapper(response.data);
+
+    // Vérifie la reponse correspond au contrat
+    if(schema){
+        return schema.parse(data);
+    }
+
+    // retourne reponse
+    return data;
+}
+
+
+/** * Exécute une la pipline en cas d'erreur*/
+async function processError<TResponse>(
+    error:unknown
+):Promise<TResponse>{
+
+    // Normalisation de toutes les erreurs en ApikitException
+    let exception:ApikitException = errorNormalizer(error);
+
+    // Point d'extension utilisateur après normalisation de l'erreur
+    const hooks = ApikitHookRegistry.get();
+    for(const hook of hooks.onError ?? []){
+        try{
+            exception = await hook(exception);
+        } catch(e){
+            exception = errorNormalizer(e);
+        }
+    }
+
+    // Gestion globale de l'erreur (UI, logs...)
+    if(shouldHandleGlobalError(exception)){
+        handleGlobalError(exception);
+    }
+
+    // Propagation de l'exception normalisée
+    throw exception;
+}
+
+/** Exécute une une requette apikit*/
 export async function apikitRequest<TResponse>(
     method: string,
     url: string,
     { payload, ...config }: RequestOptions = {},
     schema?: z.ZodSchema<TResponse>,
+
 ): Promise<TResponse> {
-    const hooks = ApikitHookRegistry.get();
-
+    const requestConfig: AxiosRequestConfig = {method, url, data:{payload}, ...config};
     try {
-        let requestConfig: AxiosRequestConfig  = {
-            method,
-            url,
-            data:{payload},
-            ...config
-        };
-        // Point d'extension utilisateur avant l'envoi de la requête
-        for(const hook of hooks.beforeRequest ?? []){
-            requestConfig = await hook(requestConfig);
-        }
-        // Exécution de la requête HTTP
-        const response = await api.request(requestConfig);
-
-
-        // Point d'extension utilisateur après une réponse réussie
-        let processedResponse = response;
-
-        for(const hook of hooks.afterResponse ?? []){
-            processedResponse = await hook(processedResponse);
-        }
-
-        // Recupere la reponse de la requette
-        const data: TResponse = apikitUnwrapper(processedResponse.data);
-
-        // Vérifie la reponse correspond au contrat
-        if (schema) {
-            return schema.parse(data);
-        }
-
-        // retourne reponse
-        return data;
-
+        return await executeRequest<TResponse>(requestConfig, schema);
     } catch (error : unknown) {
-
-        // Normalisation de toutes les erreurs en ApikitException
-        let exception:ApikitException = errorNormalizer(error);
-
-        // Point d'extension utilisateur après normalisation de l'erreur
-        for(const hook of hooks.onError ?? []){
-            try {
-                exception = await hook(exception);
-            } catch(e){
-                exception = errorNormalizer(e);
-            }
-        }
-
-        // Gestion globale de l'erreur (UI, logs...)
-        if (shouldHandleGlobalError(exception)){
-            handleGlobalError(exception);
-        }
-
-        // Propagation de l'exception normalisée
-        throw exception;
+        return await processError<TResponse>(error);
     }
 }
