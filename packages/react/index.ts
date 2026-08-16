a38ac705ba30cdf1afef7c7594becb0cf8d50b61
amine guissouma <secretwars2019@gmail.com>
2026-08-16 14:04:49 +0200

    refacto apikit: creation du package react

---
+++ packages/react/index.ts
@@ -0,0 +1,12 @@
+export * from "./src/react-apikit-error-ui-config";
+
+export * from "./src/ui_conponents/global_ui/GlobalUiProvider";
+
+export * from "./src/ui_conponents/toast/ToastService";
+export * from "./src/ui_conponents/toast/GlobalToastProvider";
+
+export * from "./src/ui_conponents/popup/PopupService";
+export * from "./src/ui_conponents/popup/GlobalPopupProvider";
+
+export * from "./src/ui_conponents/alert/AlertService";
+export * from "./src/ui_conponents/alert/GlobalAlertProvider";
